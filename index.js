0cf5423af1a94d8778b916793f6e3ee1b3a03a5d
friko21 <friedemann.koerner@gmx.de>
2017-01-13 16:17:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 var Service, Characteristic;
 var temperatureService;
 var humiditySensor;
-var temperatur = 0;
-var humidity = 0;
+var temperatur;
+var humidity;
 var url;
+var log;
+var name;
+var apikey;
+var location;
 var exec = require('child_process').exec;
+var self;
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
@@ -16,59 +21,89 @@ module.exports = function (homebridge) {
 
 function updateValues(error, stdout, stderr) {
     if (error !== null) {
-        this.log(stderr);
+        log(stderr);
     } else {
-        this.log("HTTP Response", stdout);
+        var str = new Buffer(stdout);
+        log("HTTP Response " + str.toString());
         var weatherObj = JSON.parse(stdout);
-        temperature = parseFloat(weatherObj.main.temp);
+        temperatur = parseFloat(weatherObj.main.temp);
         humidity = parseFloat(weatherObj.main.humidity);
     }
-    exec('node ./update-weather.js ' + url + ' 900000', updateValues);
+    exec('node /usr/local/lib/node_modules/homebridge-weather/update-weather.js "' + url + '" 900000', updateValues);
 }
 
-function WeatherAccessory(log, config) {
-    this.log = log;
-    this.name = config["name"];
-    this.apikey = config["apikey"];
-    this.location = config["location"];
-    url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.location + "&units=metric&APPID=" + this.apikey;
-    exec('node ./update-weather.js ' + url, updateValues);
+function WeatherAccessory(log1, config) {
+    log = log1;
+    name = config["name"];
+    apikey = config["apikey"];
+    location = config["location"];
+    temperatur = 0;
+    humidity = 0;
+    url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=" + apikey;
+    log('URL: ' + url);
+    exec('node /usr/local/lib/node_modules/homebridge-weather/update-weather.js "' + url + '"', updateValues);
+    self = this;
+}
+
+function updateState (callback, value) {
+    if (value === 'tem') {
+                        log('temperatur: ' + temperatur);
+        callback(parseFloat(temperatur));
+    }
+    if (value === 'hum') {
+                        log('humidity: ' + humidity);
+        callback(humidity);
+    }
 }
 
 WeatherAccessory.prototype =
     {
+        getStateTem: function (callback) {
+
+            this.temperatureService.setCharacteristic(Characteristic.CurrentTemperature, updateState('tem'));
+            log('getStateTem: ' + temperatur);
+            callback(null, temperatur);
+        },
+
+        getStateHum: function (callback) {
+
+            this.humiditySensor.setCharacteristic(Characteristic.CurrentRelativeHumidity, updateState('hum'));
+            log('getStateHum: ' + humidity);
+            callback(null, humidity);
+        },
+
         identify: function (callback) {
-            this.log("Identify requested!");
+            log("Identify requested!");
             callback(); // success
         },
 
         getServices: function () {
             var informationService = new Service.AccessoryInformation();
-
+            log('getServices');
             informationService
                 .setCharacteristic(Characteristic.Manufacturer, "OpenWeatherMap")
-                .setCharacteristic(Characteristic.Model, this.location)
+                .setCharacteristic(Characteristic.Model, location)
                 .setCharacteristic(Characteristic.SerialNumber, "");
 
-            temperatureService = new Service.TemperatureSensor(this.name);
-            temperatureService
+            this.temperatureService = new Service.TemperatureSensor(name);
+            this.temperatureService
                 .getCharacteristic(Characteristic.CurrentTemperature)
-                .on("get", temperature);
+                .on("get", this.getStateTem.bind(this));
 
-            temperatureService
+            this.temperatureService
                 .getCharacteristic(Characteristic.CurrentTemperature)
                 .setProps({minValue: -30});
 
-            temperatureService
+            this.temperatureService
                 .getCharacteristic(Characteristic.CurrentTemperature)
                 .setProps({maxValue: 120});
 
-            humiditySensor =  new Service.HumiditySensor(this.name);
-            humiditySensor
+            this.humiditySensor =  new Service.HumiditySensor(name);
+            this.humiditySensor
                 .getCharacteristic(Characteristic.CurrentRelativeHumidity)
- 			    .on("get", humidity);
-            
-            return [informationService, temperatureService, humiditySensor];
+ 			          .on("get", this.getStateHum.bind(this));
+            log('return');
+            return [informationService, this.temperatureService, this.humiditySensor];
         },
 
     };
